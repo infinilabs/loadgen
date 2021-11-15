a23d6fd628a0b37fc4fd02fb738494b9630b3ded
medcl <m@medcl.net>
2021-11-15 19:27:14 +0800

    support service

--- main.go
+++ main.go
@@ -190,7 +190,7 @@ func main() {
 	terminalHeader += ("/ /__/ \\_//  _  \\/ /_// /_\\\\//__/ /\\  /\n")
 	terminalHeader += ("\\____|___/\\_/ \\_/___,'\\____/\\__/\\_\\ \\/\n\n")
 
-	terminalFooter := ("Thanks for using LOADGEN, have a good day!")
+	terminalFooter := ""
 
 	app := framework.NewApp("loadgen", "A http load generator and testing suit.",
 		util.TrimSpaces(config.Version), util.TrimSpaces(config.LastCommitLog), util.TrimSpaces(config.BuildDate), util.TrimSpaces(config.EOLDate), terminalHeader, terminalFooter)
@@ -202,7 +202,7 @@ func main() {
 	global.RegisterShutdownCallback(func() {
 		//os.Exit(1)
 	})
-	app.Start(func() {
+	app.Setup(func() {
 
 		module.RegisterUserPlugin(&stats.StatsDModule{})
 		module.Start()
@@ -235,8 +235,9 @@ func main() {
 		}()
 
 	}, func() {
-	})
+	},nil)
 
+	app.Run()
 	time.Sleep(1 * time.Second)
 
 }
